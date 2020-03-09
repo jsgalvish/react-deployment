3341c5225b1e165457adf7970934fe24fbd4f3ae
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-09 12:11:58 -0500

    deplyment

---
+++ precache-manifest.1a9495eeb7342de8577cf53ddfcd60c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8ef751cea6226d1e29442011520dea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2c112e546e341695e14",
+    "url": "./static/css/main.97cac852.chunk.css"
+  },
+  {
+    "revision": "c18714de18679160370a",
+    "url": "./static/js/2.6b9c89e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.6b9c89e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c112e546e341695e14",
+    "url": "./static/js/main.6146f592.chunk.js"
+  },
+  {
+    "revision": "2720246a1e1516839192",
+    "url": "./static/js/runtime-main.bd4ad064.js"
+  }
+]);
---
+++ precache-manifest.720186fe6fa3ff5002bc251940fbe83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21b31622b3d0ae426b3cdb2be612fdf",
+    "url": "/react-deployment/index.html"
+  },
+  {
+    "revision": "d2c112e546e341695e14",
+    "url": "/react-deployment/static/css/main.97cac852.chunk.css"
+  },
+  {
+    "revision": "c18714de18679160370a",
+    "url": "/react-deployment/static/js/2.6b9c89e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-deployment/static/js/2.6b9c89e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c112e546e341695e14",
+    "url": "/react-deployment/static/js/main.6146f592.chunk.js"
+  },
+  {
+    "revision": "f381550cbf4a95b9f06b",
+    "url": "/react-deployment/static/js/runtime-main.ff106bd0.js"
+  }
+]);
